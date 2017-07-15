ac0a1e61a5b35a886fdb873ff1882b80b8341592
MuhammadRezaMahmoudi <mrphantom582@gmail.com>
2017-07-15 11:16:07 +0430

    code3

---
+++ src/MyArrayList.java
@@ -0,0 +1,46 @@
+
+public class MyArrayList<E> implements MyList<E> {
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contain(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean add(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int indexOf(E e) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
