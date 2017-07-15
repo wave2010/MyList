68082998df51c5d4bbf1fcc57baf4d086ca13122
MuhammadRezaMahmoudi <mrphantom582@gmail.com>
2017-07-15 11:13:34 +0430

    code2

--- src/MyList.java
+++ src/MyList.java
@@ -1,16 +1,20 @@
 
 public interface MyList<E> {
 
-	
 	int size();
-	boolean isEmpty();
-	boolean contain(E e);
-	boolean add(E e);
-	boolean remove(E e);
-	void clear();
-	boolean equals(Object o);
-	int indexOf(E e);
 
-	
+	boolean isEmpty();
+
+	boolean contain(E e);
+
+	boolean add(E e);
+
+	boolean remove(E e);
+
+	void clear();
+
+	boolean equals(Object o);
+
+	int indexOf(E e);
 
 }
